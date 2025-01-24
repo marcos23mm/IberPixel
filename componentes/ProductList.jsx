098f4ddb52b083cdc038429ec8b6efbf6837cdbb
marcos23mm <161719402+marcos23mm@users.noreply.github.com>
2025-01-24 13:06:34 +0100

    Add files via upload

---
+++ componentes/ProductList.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './ProductList.css';
+
+const ProductList = ({ products }) => {
+  return (
+    <div className="product-grid">
+      {products.map((product) => (
+        <div className="product-card" key={product.id}>
+          <img src={product.image} alt={product.title} className="product-image" />
+          <h3 className="product-title">{product.title}</h3>
+          <p className="product-price">${product.price}</p>
+          {/* 
+          <p className="product-category">{product.category}</p> 
+          */}
+
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
+
